028130de306486814dca9decc6f91632a175f0a9
shiro16 <nyanyanyawan24@gmail.com>
2015-09-03 23:55:50 +0900

    fixed spec

--- spec/support/setup_database.rb
+++ spec/support/setup_database.rb
@@ -1,4 +1,4 @@
-ctiveRecord::Base.configurations = {'test' => {adapter: 'sqlite3', database: ':memory:'}}
+ActiveRecord::Base.configurations = {'test' => {adapter: 'sqlite3', database: ':memory:'}}
 ActiveRecord::Base.establish_connection :test
 
 class CreateAllTables < ActiveRecord::Migration
